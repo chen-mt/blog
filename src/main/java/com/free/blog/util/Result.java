390e19acfcba7d4d57b01ddf20a116280c238b28
chenmt <2252540056@qq.com>
2020-03-15 21:41:40 +0800

    result util commit...

--- src/main/java/com/free/blog/util/Result.java
+++ src/main/java/com/free/blog/util/Result.java
@@ -1,19 +1,19 @@
 package com.free.blog.util;
 
-import lombok.Data;
+        import lombok.Data;
 
 @Data
 public class Result {
 
     private Integer errorCode;//错误码，1表示成功，0表示失败
-    private String msg;//错误信息
+    private Object data;//返回信息
 
     public Result() {
     }
 
-    public Result(Integer errorCode, String msg) {
+    public Result(Integer errorCode, Object data) {
         this.errorCode = errorCode;
-        this.msg = msg;
+        this.data = data;
     }
 
 }
